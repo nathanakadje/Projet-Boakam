7ebe7b9b8d867994e0bf7b30369cb066d33b0825
Nathan <christakadje20@gmail.com>
2025-02-26 21:35:24 +0000

    26/02/2025 21:35

--- database/migrations/2025_02_12_161903_add_montant_to_reservations.php
+++ database/migrations/2025_02_12_161903_add_montant_to_reservations.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('reservations', function (Blueprint $table) {
-            $table->integer('montant')->unsigned()->nullable();            //
+            $table->integer('montant')->unsigned()->nullable();           
         });
     }
 
