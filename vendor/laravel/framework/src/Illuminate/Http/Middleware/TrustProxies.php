110268de7aa3b1e256f9081b44a56a300184ae86
nathan akadje <christakadje@gmail.com>
2025-06-11 23:40:50 +0000

    commit 12/06/25

--- vendor/laravel/framework/src/Illuminate/Http/Middleware/TrustProxies.php
+++ vendor/laravel/framework/src/Illuminate/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The trusted proxies headers for the application.
